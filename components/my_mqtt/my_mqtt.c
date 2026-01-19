3c3cd36c500ba1d46fae222a0650e47f7e86cf33
ThiagoTM30 <thiago.munck@estudante.ufjf.br>
2026-01-19 15:12:57 -0300

    deu certo

--- components/my_mqtt/my_mqtt.c
+++ components/my_mqtt/my_mqtt.c
@@ -4,67 +4,53 @@
 #include <string.h>
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_wifi.h"
 #include "esp_netif.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
-
+#include "freertos/queue.h"
 #include "lwip/sockets.h"
 #include "lwip/dns.h"
 #include "lwip/netdb.h"
-
 #include "esp_log.h"
 #include "mqtt_client.h"
-
 #include "my_mqtt.h"
+
+// URL DO BROKER PARA TESTE
+#define MQTT_URL "mqtt://test.mosquitto.org:1883" 
 
 static const char *TAG = "MQTT";
 
 extern SemaphoreHandle_t mqttconnectedSemaphore;
-extern QueueHandle_t filaData;
+// extern QueueHandle_t filaData; // Comentado pois não existe no main
 
 esp_mqtt_client_handle_t client;
 
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
-                               int32_t event_id, void *event_data)
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     esp_mqtt_event_handle_t event = event_data;
 
     switch ((esp_mqtt_event_id_t)event_id)
     {
     case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT Conectado!");
         xSemaphoreGive(mqttconnectedSemaphore);
         break;
 
-    case MQTT_EVENT_DATA:
-    {
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\n", event->data_len, event->data);
-
-        // --- COPIAR DADOS PARA UM BUFFER PRÓPRIO ---
-        char buffer[100];
-        int len = event->data_len;
-
-        if (len >= sizeof(buffer))
-            len = sizeof(buffer) - 1;
-
-        memcpy(buffer, event->data, len);
-        buffer[len] = '\0';
-
-        // Enviar para fila
-        xQueueSendToBack(filaData, buffer, portMAX_DELAY);
-    }
-    break;
-
     case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        ESP_LOGI(TAG, "MQTT Desconectado");
         break;
 
     case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        ESP_LOGD(TAG, "Mensagem publicada, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "Dado Recebido!");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        // A lógica de enviar para fila foi removida temporariamente 
+        // para evitar erros de compilação, já que o main não processa recepção ainda.
         break;
 
     case MQTT_EVENT_ERROR:
@@ -72,7 +58,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
         break;
 
     default:
-        ESP_LOGI(TAG, "Other event id: %d", event->event_id);
+        ESP_LOGI(TAG, "Outro evento id: %d", event->event_id);
         break;
     }
 }
@@ -80,25 +66,26 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
 void mqtt_start()
 {
     esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = CONFIG_BROKER_URL,
+        .broker.address.uri = MQTT_URL,
     };
 
     client = esp_mqtt_client_init(&mqtt_cfg);
-
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID,
-                                   mqtt_event_handler, NULL);
-
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
 }
 
 void mqtt_publish(char *topic, char *msg)
 {
-    int msg_id = esp_mqtt_client_publish(client, topic, msg, 0, 1, 0);
-    ESP_LOGI(TAG, "Mensagem Enviada! ID = %d", msg_id);
+    if (client) {
+        int msg_id = esp_mqtt_client_publish(client, topic, msg, 0, 1, 0);
+        ESP_LOGI(TAG, "Enviando: %s, ID: %d", msg, msg_id);
+    }
 }
 
-void mqtt_sbscribe(char *topic)
+void mqtt_subscribe(char *topic)
 {
-    int msg_id = esp_mqtt_client_subscribe(client, topic, 0);
-    ESP_LOGI(TAG, "Subscrito no Tópico %s! ID = %d", topic, msg_id);
+    if (client) {
+        int msg_id = esp_mqtt_client_subscribe(client, topic, 0);
+        ESP_LOGI(TAG, "Subscrito em %s, ID: %d", topic, msg_id);
+    }
 }
